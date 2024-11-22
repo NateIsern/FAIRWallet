73413d2f5f9d0cb67108a7852e79aa0439efef6d
Nate Isern Alvarez <nate.isern.alvarez@gmail.com>
2024-11-22 19:22:27 +0100

    Add a new file to define the `faircoinNet` constant
    * Define the `faircoinNet` object with properties: `messagePrefix`, `bech32`, `bip32`, `pubKeyHash`, `scriptHash`, and `wif`

---
+++ constants/faircoinNet.ts
@@ -0,0 +1,8 @@
+const faircoinNet = {
+    messagePrefix: "\x18Faircoin Signed Message:\n",
+    bech32: "tb",
+    bip32: { public: 70617039, private: 70615956 },
+    pubKeyHash: 0x23,
+    scriptHash: 0xc4,
+    wif: 0xd4,
+};
